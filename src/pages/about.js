7ba7111f3b4d071c44f8d967da2a6153f4576a00
misfitonie <misfitonie@gmail.com>
2023-07-28 10:02:58 +0200

    modify animated number

--- src/pages/about.js
+++ src/pages/about.js
@@ -82,21 +82,21 @@ const About = () => {
                                 +
                             </span>
                             <h2 className='text-xl font-medium capitalize text-colors-dark/75 dark:text-colors-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm'>
-                                femmes satisfaites</h2>
+                                prototypes réalisé</h2>
                         </div>
 
                         <div className='flex flex-col items-end justify-center xl:items-center'>
                             <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
                                 <AnimatedNumbers value={50}/>
-                                +
+                                
                             </span>
                             <h2 className='text-xl font-medium capitalize text-colors-dark/75 dark:text-colors-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm'>
-                                oscars</h2>
+                                années d'expérience</h2>
                         </div>
 
                         <div className='flex flex-col items-end justify-center xl:items-center'>
                             <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
-                                <AnimatedNumbers value={50}/>
+                                <AnimatedNumbers value={0}/>
                                 €
                             </span>
                             <h2 className='text-xl font-medium capitalize text-colors-dark/75 dark:text-colors-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm'>
