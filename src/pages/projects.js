b2beb33bf230c03dd562d69c75bd7a885ca51db6
Théo Ferreira <ferreiratheo.pro@gmail.com>
2023-08-02 22:11:12 +0200

    add portefolio project

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -5,7 +5,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import project1 from "../../public/images/projects/crypto-screener-cover-image.jpg"
+import project1 from "../../public/images/projects/portefoliopic.png"
 import project2 from "../../public/images/projects/KR.jpg"
 import project3 from "../../public/images/projects/D&R.jpg"
 import { motion } from 'framer-motion'
@@ -94,14 +94,12 @@ const projects = () => {
           <div className='grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0'>
               <div className='col-span-12'>
                 <FeaturedProject
-                title="Crypto Screener Application"
-                summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
-                It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
-                local currency."
-                link="/"
+                title="Portefolio"
+                summary="Vous pouvez retrouver l'intégralité du code de mon portefolio sur mon Github, vous y trouverez également la plupart de mes autres projets."
+                link="https://github.com/misfitonie/Portefolio"
                 type="Featured Project"
                 img={project1}
-                githublink="/"
+                githublink="https://github.com/misfitonie/Portefolio"
                 />
               </div>
               <div className='col-span-6 sm:col-span-12'>
